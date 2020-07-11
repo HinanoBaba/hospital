5c5ce3717ad79ff2bc264a70ec36cf80bf74943c
HinanoBaba <67220675+HinanoBaba@users.noreply.github.com>
2020-07-11 23:03:27 +0900

    Create menu-naika.php

---
+++ menu-naika.php
@@ -0,0 +1,51 @@
+<?php 
+$title = "佐藤病院|診察案内-内科";
+?>
+
+<?php
+include "./parts/header.php";
+?>
+
+<article>
+	<section>
+		<h3 class="text-center">一般内科</h3>
+		<p class="text-center">風邪、頭痛、息苦しさ、胸焼け、腹痛、インフルエンザ、花粉症、などの診療を行っております。
+		総合内科専門医ですので体の不調ばかりではなく介護、その他なんでもご相談下さい。</p>
+	</section>
+	
+	<section>
+		<h3 class="text-center">生活習慣病</h3>
+		<p class="text-center">高血圧・高脂血症・糖尿病などに関しては、最近のEBMに準じた治療を行うと共に、患者様の環境、家族構成および仕事なども理解して最も良い治療方法を選択しています。特に、糖尿病に対してはお互いの信頼関係を築くところから始めています。
+		また最近は、インスリン注射の導入も容易になり、治療をすすめています。</p>
+	</section>
+
+	<section>
+		<h3 class="text-center">糖尿病</h3>
+		<p class="text-center">糖尿病は、生活習慣病の1つです。血液の中の糖分(血糖)が多すぎる状態が続く病気です。血糖は膵臓(すいぞう)から出るインスリンというホルモンにより調節されますが、出るインスリンの量が足りなくなったり、効き目が悪くなると糖尿病になります。
+		高血糖が続くと血管が傷つき、身体のあちこちに障害が発生します。腎臓の機能が失われる腎症、失明に至る網膜症、脳梗塞や心筋梗塞を招く動脈硬化も進行します。
+		糖尿病は恐ろしい病気ですが、よほど重症にならない限り、自覚症状がほとんどないのも糖尿病の特徴です。定期的な検査で身体の状態を知って、的確な治療を行うことが必要です。</p>
+		<h4 class="text-center">原因</h4>
+		<p class="text-center">糖尿病を起こしやすい体質の方が、食べすぎ・運動不足・ストレスといった環境因子が加わった時にインスリンの作用不足をおこし、糖尿病を発症すると考えられています。（糖尿病にはインスリンそのものがでなくなるタイプもあります）</p>
+		<h4 class="text-center">こんな症状はありませんか？もしもあったら、検査を受けてみましょう。</h4>
+		<div class="d-flex justify-content-center">
+		<ul>
+			<li><i class="fas fa-check"></i>20才代の時よりかなり太った。</li>
+			<li><i class="fas fa-check"></i>朝食は食べない。</li>
+			<li><i class="fas fa-check"></i>夕食が遅く、食べたらすぐに寝る。</li>
+			<li><i class="fas fa-check"></i>早食いである。</li>
+			<li><i class="fas fa-check"></i>油っこいものが好き。</li>
+			<li><i class="fas fa-check"></i>お菓子やジュースが好きで、食事の合間に食べている。</li>
+			<li><i class="fas fa-check"></i>お酒を毎晩飲む。</li>
+			<li><i class="fas fa-check"></i>運動はほとんどしない。</li>
+			<li><i class="fas fa-check"></i>眼がかすむ。</li>
+			<li><i class="fas fa-check"></i>手足がしびれる。</li>
+			<li><i class="fas fa-check"></i>たくさん食べるのに痩せてきた。</li>
+			<li><i class="fas fa-check"></i>すごく喉が渇く。</li>
+			<li><i class="fas fa-check"></i>家族に糖尿病の人がいる。</li>
+		</ul>
+		</div>
+	</section>
+</article>
+<?php
+include "./parts/footer.php";
+?>
