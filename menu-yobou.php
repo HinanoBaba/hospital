cf26e9988a53859f76d49959a3a5d1a2d102a9f4
HinanoBaba <67220675+HinanoBaba@users.noreply.github.com>
2020-07-11 23:05:38 +0900

    Create menu-yobou.php

---
+++ menu-yobou.php
@@ -0,0 +1,287 @@
+<?php 
+$title = "佐藤病院|診察案内-各種予防接種";
+?>
+
+<?php
+include "./parts/header.php";
+?>
+<article>
+  <section>
+    <h3 class="text-center">各種予防接種</h3>
+    <p class="text-center">インフルエンザや風疹など18種の予防接種が接種可能です。</p>
+    <p class="text-center"><small>クリックで詳細にジャンプします。</small></p>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou01">二種混合（DT)</a></li>
+    	<li><a href="#yobou02">三種混合（DPT）</a></li>
+    	<li><a href="#yobou03">四種混合（DPT-IPV）</a></li>
+    </ul>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou04">麻しん（単抗体）</a></li>
+    	<li><a href="#yobou05">風しん（単抗体）</a></li>
+    	<li><a href="#yobou06">麻しん・風しん（MR)</a></li>
+    </ul>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou07">不活化ポリオ</a></li>
+    	<li><a href="#yobou08">ヒブ</a></li>
+    	<li><a href="#yobou09">ロタウイルス</a></li>
+    </ul>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou10">日本脳炎</a></li>
+    	<li><a href="#yobou11">おたふくかぜ</a></li>
+    	<li><a href="#yobou12">みずぼうそう</a></li>
+    </ul>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou13">B型肝炎</a></li>
+    	<li><a href="#yobou14">A型肝炎</a></li>
+    	<li><a href="#yobou15">子宮頸がん</a></li>
+    </ul>
+    <ul class="d-flex justify-content-between">
+    	<li><a href="#yobou16">小児用肺炎球菌</a></li>
+    	<li><a href="#yobou17">肺炎球菌（65歳以上）</a></li>
+    	<li><a href="#yobou18">インフルエンザ</a></li>
+    </ul>
+  </section>
+
+  <section id="yobou01" class="p-3 mb-2 bg-light border border-info">
+    <h3>二種混合（DT)</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>二種混合ワクチン（DT）</dd>
+      <dt>予防する病気</dt>
+      <dd>ジフテリア、破傷風</dd>
+      <dt>注意点</dt>
+      <dd>小児のときに未接種の成人は、破傷風単独ないし二種混合ワクチンを接種。6カ月間で3回接種。その後10年に1度は追加接種を受けた方がよいとされています。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou02" class="p-3 mb-2 bg-light border border-dark">
+    <h3>三種混合ワクチン（DPT）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>三種混合ワクチン（DPT）</dd>
+      <dt>予防する病気</dt>
+      <dd>ジフテリア、百日せき、破傷風</dd>
+      <dt>注意点</dt>
+      <dd>接種の効果は約10年。10年経過後、破傷風単独ないし二種混合ワクチンを接種します</dd>
+    </dl>
+  </section>
+
+  <section id="yobou03" class="p-3 mb-2 bg-light border border-dark">
+    <h3>四種混合ワクチン（DPT-IPV）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>四種混合ワクチン（DPT-IPV）</dd>
+      <dt>予防する病気</dt>
+      <dd>ジフテリア、百日せき、破傷風、ポリオ</dd>
+      <dt>注意点</dt>
+      <dd>2012年8月以降に誕生した乳幼児や、これまでに三種混合（DPT）とポリオワクチンを一度も接種していない場合には、原則として四種混合（DPT- IPV）ワクチンを接種します。2012年7月以前に誕生した子どもで、すでに三種混合（DPT）ワクチンを接種している場合は、原則として四種混合ワク チンではなく、三種混合ワクチンを4回受けます。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou04" class="p-3 mb-2 bg-light border border-dark">
+    <h3>麻しん（単抗体）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>麻しんワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>麻しん（はしか）</dd>
+      <dt>注意点</dt>
+      <dd>麻しん（はしか）は子供のときに罹ると熱と発疹が出る病気です。大人になってから罹ると肺炎や脳炎になることが少なくなく、重症になる病気です。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou05" class="p-3 mb-2 bg-light border border-dark">
+    <h3>風しん（単抗体）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>風しんワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>ジフテリア、破傷風</dd>
+      <dt>注意点</dt>
+      <dd>小児のときに未接種の成人は、破傷風単独ないし二種混合ワクチンを接種。6カ月間で3回接種。その後10年に1度は追加接種を受けた方がよいとされています。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou06" class="p-3 mb-2 bg-light border border-dark">
+    <h3>MR（麻しん・風しん混合ワクチン）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>MR（麻しん・風しん混合ワクチン）</dd>
+      <dt>予防する病気</dt>
+      <dd>麻しん（はしか）、風しん</dd>
+      <dt>注意点</dt>
+      <dd>大人でもかかるため、30代前半までで2回接種を受けてない人はぜひ接種しましょう。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou07" class="p-3 mb-2 bg-light border border-dark">
+    <h3>不活化ポリオ</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>不活化ポリオワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>ポリオ（小児まひ、急性灰白脊髄炎)</dd>
+      <dt>注意点</dt>
+      <dd>生後3か月から接種できます。3～8週間隔で3回、3回目の約1年後（6か月後から接種可能）に4回目を接種します。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou08" class="p-3 mb-2 bg-light border border-dark">
+    <h3>ヒブ</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>ヒブワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>ヒブ感染症</dd>
+      <dt>注意点</dt>
+      <dd>生後2か月から接種できます。標準的なスケジュールでは、4～8週間隔で3回、3回目から7か月以上あけて1歳すぐに4回目を接種します。初回の接種月齢・年齢によって接種間隔・回数が異なります。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou09" class="p-3 mb-2 bg-light border border-dark">
+    <h3>ロタウイルス</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>ロタウイルスワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>ロタウイルス感染症</dd>
+      <dt>注意点</dt>
+      <dd>ロタウイルスワクチンは飲むタイプの生ワクチンのため、接種後に4週間以上間隔をあけなければ次のワクチンを接種できません。0歳児はほかにも接種が必要なワクチンが多数ありますので、同時接種で受けることが重要です。生後2か月になったらすぐにヒブ、小児用肺炎球菌ワクチンなどと同時接種で受けることをおすすめします。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou10" class="p-3 mb-2 bg-light border border-dark">
+    <h3>日本脳炎</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>日本脳炎ワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>日本脳炎</dd>
+      <dt>注意点</dt>
+      <dd>日本脳炎ウイルスを持った蚊により国内でも感染の可能性があります。感染すると死亡率は20〜40%にのぼります。治療法はなく予防が最も大切な疾患です</dd>
+    </dl>
+  </section>
+
+  <section id="yobou11" class="p-3 mb-2 bg-light border border-dark">
+    <h3>おたふくかぜ</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>おたふくかぜワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>おたふくかぜ</dd>
+      <dt>注意点</dt>
+      <dd>主に子供に流行しやすい病気ですが、中学生以降の大人でも感染すると症状が重く、睾丸炎・髄膜炎・永久難聴という合併症を併発することもある危険な病気です。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou12" class="p-3 mb-2 bg-light border border-dark">
+    <h3>みずぼうそう</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>みずぼうそうワクチン（水痘ワクチン）</dd>
+      <dt>予防する病気</dt>
+      <dd>みずぼうそう（水痘）</dd>
+      <dt>注意点</dt>
+      <dd>妊婦、免疫機能が低下している人、骨髄やリンパ系の癌がある人には接種できません。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou13" class="p-3 mb-2 bg-light border border-dark">
+    <h3>B型肝炎</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>B型肝炎ワクチン（大人用）</dd>
+      <dt>予防する病気</dt>
+      <dd>B型肝炎</dd>
+      <dt>注意点</dt>
+      <dd>3回の接種が必要で、1回目と2回目は1〜2か月開け、3回目は1回目から4〜6か月後に接種します。</dd>
+    </dl>
+
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>B型肝炎ワクチン（子供用）</dd>
+      <dt>予防する病気</dt>
+      <dd>B型肝炎</dd>
+      <dt>注意点</dt>
+      <dd>生後すぐから接種可能（一ヶ月検診での接種可能）。4週間隔で2回、その後20－24週経ってから1回の合計3回接種。できれば生後1－3か月からがおすすめ。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou14" class="p-3 mb-2 bg-light border border-dark">
+    <h3>A型肝炎</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>A型肝炎ワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>A型肝炎</dd>
+      <dt>注意点</dt>
+      <dd>半年以上間隔を取って2回接種します。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou15" class="p-3 mb-2 bg-light border border-dark">
+    <h3>子宮頸がん</h3>
+    <p>子宮頸がんを引き起こすHPV(ヒトパピローマウィルス)に対する予防ワクチンには、「2価ワクチン｣と「4価ワクチン」の2種類あります。
+
+    2価ワクチンと4価ワクチンの最大の違いは、4価ワクチンの方が予防できるウイルスの「型」が多い、つまり予防できる病気の種類が多いという点です。ただ、子宮頸がんの予防効果は、ほぼ同じで、あまり差はないと言えます。
+
+    4価ワクチンは、HPV6型や11型も予防できるため、HPV由来の病変である「尖圭コンジローマ」をはじめとする幅広い病気の予防につながります。</p>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>子宮頸がんワクチン（2価HPVワクチン）</dd>
+      <dt>予防する病気</dt>
+      <dd>子宮頸がん</dd>
+      <dt>注意点</dt>
+      <dd>子宮頸がん発症の高リスクグループである、HPV16型、HPV18型を予防。</dd>
+    </dl>
+
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>子宮頸がんワクチン（4価HPVワクチン）</dd>
+      <dt>予防する病気</dt>
+      <dd>子宮頸がん、尖圭コンジローマ</dd>
+      <dt>注意点</dt>
+      <dd>HPV16型、18型の他に、低リスクグループのHPV６型と11型を予防。低リスクグループのHPVは子宮頸がん発症は稀で、尖圭コンジローマの発症に関与。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou16" class="p-3 mb-2 bg-light border border-dark">
+    <h3>小児用肺炎球菌</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>小児用肺炎球菌ワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>小児の肺炎球菌感染症（細菌性髄膜炎、肺炎など）</dd>
+      <dt>注意点</dt>
+      <dd>生後2か月から接種できます。標準的なスケジュールでは4週間隔で3回、生後12～15か月齢に4回目を接種します。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou17" class="p-3 mb-2 bg-light border border-dark">
+    <h3>肺炎球菌（65歳以上）</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>肺炎球菌ワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>肺炎球菌感染症（細菌性髄膜炎・菌血症、菌血症を伴う肺炎など）</dd>
+      <dt>注意点</dt>
+      <dd>65歳以下で接種した人は65歳を過ぎて再接種します。5年以内の接種は出来ません。</dd>
+    </dl>
+  </section>
+
+  <section id="yobou18" class="p-3 mb-2 bg-light border border-dark">
+    <h3>インフルエンザ</h3>
+    <dl>
+      <dt>ワクチン名</dt>
+      <dd>インフルエンザワクチン</dd>
+      <dt>予防する病気</dt>
+      <dd>インフルエンザウイルス感染</dd>
+      <dt>注意点</dt>
+      <dd>予防接種の後まれに副反応が起こることがあります。また、予防接種と同時に、ほかの病気がたまたま重なって現れることがあります。予防接種を受けた後、接種した部位が痛みや熱をもってひどくはれたり、全身のじんましん、繰り返す嘔吐、顔色の悪さ、低血圧、高熱などが現れたら診療を受けてください。</dd>
+    </dl>
+  </section>
+</article>
+
+<?php
+include "./parts/footer.php";
+?>
